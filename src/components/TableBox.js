0034496a89da246059af00f98475cdd9d469190b
richardhan <hxy.314@gmail.com>
2019-09-19 21:53:35 +1000

    refactoring table ui

--- src/components/TableBox.js
+++ src/components/TableBox.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import TableStatus from './TableStatus';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
 
+const Wrapper = styled.div`
+	display: flex;
+	width: 100%;
+	margin-bottom: 30px;
+
+	.MuiTableCell-head {
+		background-color: lightblue;
+		color: rgba(0, 0, 0, 0.87);
+	}
+`;
+
 const TableBox = ({ busData }) => {
-	const showTableRow = busData.map(item => {
+	const showTableRow = busData.map(showTableRow => {
 		return (
-			<div key={item.busId}>
-				{item.busId}
-				{item.routeVariant}
-				<TableStatus status={item.deviationFromTimetable} />
-			</div>
+			<TableRow key={showTableRow.busId}>
+				<TableCell className="tableCell-busId">{showTableRow.busId}</TableCell>
+				<TableCell className="tableCell-routeVariant">{showTableRow.routeVariant}</TableCell>
+				<TableCell className="tableCell-deviationFromTimetable">
+					<TableStatus status={showTableRow.deviationFromTimetable} />
+				</TableCell>
+			</TableRow>
 		);
 	});
-	return <div>{showTableRow}</div>;
+
+	return (
+		<Wrapper className="table">
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>
+							<Typography variant="h6" component="p">
+								Bus ID
+							</Typography>
+						</TableCell>
+						<TableCell>
+							<Typography variant="h6" component="p">
+								Route Variant
+							</Typography>
+						</TableCell>
+						<TableCell>
+							<Typography variant="h6" component="p">
+								Status
+							</Typography>
+						</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>{showTableRow}</TableBody>
+			</Table>
+		</Wrapper>
+	);
 };
 
 TableBox.propTypes = {
