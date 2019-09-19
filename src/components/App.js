cba80b0a8b7770bf556ee33d711ec65dd6629b81
richardhan <hxy.314@gmail.com>
2019-09-19 21:52:11 +1000

    refactor main container ui

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
 import Home from '../container/Home';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	margin-top: 30px;
+	margin-bottom: 30px;
+`;
 
 function App() {
 	return (
-		<Container maxWidth="sm">
-			<Home />
-		</Container>
+		<Wrapper>
+			<Container maxWidth="sm">
+				<Home />
+			</Container>
+		</Wrapper>
 	);
 }
 
